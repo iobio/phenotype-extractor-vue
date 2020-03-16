c9e1cbc4aca7e7418d1060dbe4ec70cb34a0af6d
Aditya Ekawade <aditya.ekawade@gmail.com>
2020-03-16 16:33:41 -0700

    change delete gene wording

--- src/components/GeneList.vue
+++ src/components/GeneList.vue
@@ -168,7 +168,7 @@
           <v-card-actions>
             <v-spacer></v-spacer>
             <v-btn color="primary" text @click="cancelDeleteGene">Cancel</v-btn>
-            <v-btn color="primary" text @click="deleteGene">Agree</v-btn>
+            <v-btn color="primary" text @click="deleteGene">Yes</v-btn>
 
           </v-card-actions>
         </v-card>
